d93a43757c519f131b03455e6fd2d25ee608cc6c
Mica <mika89700@gmail.com>
2023-08-18 20:51:48 +0200

    raise exception

--- tests/test_quantum_glasses.py
+++ tests/test_quantum_glasses.py
@@ -9,6 +9,7 @@ class TestQuantumGlasses(unittest.TestCase):
         my_qglases = QuantumGlasses()
 
         if os.getenv('DISPLAY') is None:
+            os.environ.__setitem__('DISPLAY', ':0.0')
             with self.assertRaises(Exception):
                 my_qglases.main(testing=True)
 
